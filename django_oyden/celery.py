f87a38f684ba7b6126206099a4a06bcc9730cc5a
arslans-repo <arslan092486@gmail.com>
2020-08-14 20:16:24 +0500

    first commit

--- django_oyden/celery.py
+++ django_oyden/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 
 # Zadayem peremennuyu okrujeniya, soderjashuyu nazvaniye fayla nastroyek nashego proyekta
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_oyde.settings')
-app = Celery('django_oyde')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_oyden.settings')
+app = Celery('django_oyden')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- orders/tasks.py
+++ orders/tasks.py
@@ -5,12 +5,13 @@ from .models import Order, OrderItem
 
 @task
 def order_created(order_id):
+
     """ Zadacha otpravki email-uvedomleniy pri uspeshnom oformlenii zakaza."""
     order = Order.objects.get(id=order_id)
 
-    subject = 'Zakazynyzyn belligi nr. {}'.format(order_id)
+    subject = 'Номер вашего заказа. {}'.format(order_id)
     message = 'Gadyrly {},\n\nSizin satyn almak uchin eden onumleriniz bellige alyndy .' \
-              '\nSizin zakazynyzyn belligi {}.'.format(order.first_name, order.id)
+              '\nSizin zakazynyzyn belligi {}.'.format(order.first_name , order.id)
     mail_sent = send_mail(subject,
                           message,
                           'oydencom@gmail.com',
